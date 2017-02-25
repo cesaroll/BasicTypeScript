a966dd4da2ac7e37a24c66be2e620d5da589a411
Cesar Lopez <cesarl@outlook.com>
2017-02-24 19:30:16 -0600

    Function returnning void

---
+++ functions.js
@@ -0,0 +1,4 @@
+function greet(firstName, lastName) {
+    console.log("Hello " + firstName + " " + lastName);
+}
+greet("Cesar", "Lopez");
---
+++ functions.ts
@@ -0,0 +1,5 @@
+function greet(firstName : string, lastName : string) : void {
+    console.log(`Hello ${firstName} ${lastName}`);
+}
+
+greet("Cesar", "Lopez");
